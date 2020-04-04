c31c0e5c06fd922134d520b2184b84b63ffa72ad
geedhaipriyan <geedhaipriyan1998@gmail.com>
2020-04-04 10:30:03 +0530

    initial commit

---
+++ Main_temp.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define ll long long int 
+#define f(min,max) for(ll i=min;i<=max;i++)
+#define f1(min,max) for(ll j=min;j<=max;j++)
+#define pair1 pair<ll,ll>
+using namespace std;
+int main()
+{
+  ll n;
+  return 0;   
+}
